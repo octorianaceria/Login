8eca490ed9f823cccc104fcc13dd8d1fedaa8fb9
feniaster <ceriap860@gmail.com>
2023-06-28 14:06:34 +0700

    a

---
+++ app/src/main/java/com/octoriana201103650/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.octoriana201103650.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
