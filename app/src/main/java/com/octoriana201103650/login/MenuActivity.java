b4344079275a0bc09fbd6fd76a835baa303b08c6
feniaster <ceriap860@gmail.com>
2023-07-07 13:44:50 +0700

    a

--- app/src/main/java/com/octoriana201103650/login/MenuActivity.java
+++ app/src/main/java/com/octoriana201103650/login/MenuActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private Button _tampilMahasiswaButton, _tampilForexButton;
-    private Intent _tampilMahasiswaIntent, _tampilForexIntent;
+    private Button _tampilMahasiswaButton, _tampilForexButton, _tampilCuacaButton;
+    private Intent _tampilMahasiswaIntent, _tampilForexIntent, _tampilCuacaIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,7 @@ public class MenuActivity extends AppCompatActivity {
 
         initTampilMahasiswaButton();
         initTampilForexButton();
+        initTampilCuacaButton();
     }
 
     private void initTampilMahasiswaButton() {
@@ -41,6 +42,19 @@ public class MenuActivity extends AppCompatActivity {
             public void onClick(View view) {
                 _tampilForexIntent = new Intent(getApplicationContext(), ForexMainActivity.class);
                 startActivity(_tampilForexIntent);
+            }
+        });
+    }
+
+    private void initTampilCuacaButton()
+    {
+        _tampilCuacaButton = findViewById(R.id.tampilCuacaButton);
+
+        _tampilCuacaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _tampilCuacaIntent = new Intent(getApplicationContext(), CuacaMainActivity.class);
+                startActivity(_tampilCuacaIntent);
             }
         });
     }
